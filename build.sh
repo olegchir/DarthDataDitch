7add46351b2e3b544d398a5f489f005af71dd2fe
Andrey Kononov <zloykaban@gmail.com>
2023-10-07 00:08:56 +0400

    added some build options

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pwd
 if [ -f .env ]; then
-    docker build -f docker/Dockerfile --build-arg USE_ENV=.env -t darthdataditch:dev .
+    docker build -f docker/Dockerfile.dev --build-arg USE_ENV=.env -t darthdataditch:dev .
 else
     docker build --no-cache --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) -f docker/Dockerfile -t darthdataditche:prod .
 fi
