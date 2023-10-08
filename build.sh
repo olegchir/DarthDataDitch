d2082a125a913791b8e1892481f347276f9968a4
Andrey Kononov <zloykaban@gmail.com>
2023-10-08 12:28:16 +0400

    added ecr support and modified build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-pwd
+ECR_URL="433663489437.dkr.ecr.eu-central-1.amazonaws.com/darthdataditch-ecr"
+
+$(aws ecr get-login --no-include-email --region <your-region>)
+
 if [ -f .env ]; then
     docker build -f docker/Dockerfile.dev --build-arg USE_ENV=.env -t darthdataditch:dev .
+    docker tag darthdataditch:dev ${ECR_URL}:dev
+    docker push ${ECR_URL}:dev
 else
-    docker build --no-cache --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) -f docker/Dockerfile -t darthdataditche:prod .
+    docker build --no-cache --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) -f docker/Dockerfile -t darthdataditch:prod .
+    docker tag darthdataditch:prod ${ECR_URL}:prod
+    docker push ${ECR_URL}:prod
 fi
--- terraform/global/iam_policy.tf
+++ terraform/global/iam_policy.tf
@@ -25,3 +25,28 @@ resource "aws_iam_policy" "s3_holocron-archives_policy" {
 output "s3_put_get_list_policy_arn" {
   value = aws_iam_policy.s3_holocron-archives_policy.arn
 }
+
+resource "aws_ecr_lifecycle_policy" "darthdataditch" {
+  repository = aws_ecr_repository.darthdataditch.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+output "aws_ecr_lifecycle_policy_darthdataditch_arn" {
+  value = aws_iam_policy.s3_holocron-archives_policy.arn
+}
