3e63f5d5e6dfadcc90e53269fd7dea6a1e07ff96
Andrey Kononov <zloykaban@gmail.com>
2023-10-12 18:46:15 +0400

    adjust build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ECR_URL="433663489437.dkr.ecr.eu-central-1.amazonaws.com/darthdataditch-ecr"
-APP_VERSION=$(grep '^version:' helm/darthdataditch/Chart.yaml | awk '{print $2}')
+APP_VERSION=$(grep '^appVersion:' helm/darthdataditch/Chart.yaml | awk '{print $2}')
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin ${ECR_URL}
 docker build --no-cache --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) -f docker/Dockerfile -t darthdataditch:prod .
 docker tag darthdataditch:prod ${ECR_URL}:${APP_VERSION}
