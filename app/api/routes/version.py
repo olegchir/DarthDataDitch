a82c879a47e47fe4f1f06ab4bbede30aa1c8ebf0
Andrey Kononov <zloykaban@gmail.com>
2023-10-12 18:25:30 +0400

    update /version endpoint

--- app/api/routes/version.py
+++ app/api/routes/version.py
@@ -1,8 +1,37 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 from app.utils.config import APP_VERSION
 
 router = APIRouter()
 
 @router.get("/version")
 def version():
-    return {"version": APP_VERSION}
+    version = f"""
+
+    Welcome to the Star Wars API! ver. {APP_VERSION}
+    
+```
+
+          __
+.-.__      \ .-.  ___  __
+|_|  '--.-.-(   \/\;;\_\.-._______.-.
+(-)___     \ \ .-\ \;;\(   \       \ \
+ Y    '---._\_((Q)) \;;\\ .-\     __(_)
+ I           __'-' / .--.((Q))---'    \,
+ I     ___.-:    \|  |   \'-'_          \
+ A  .-'      \ .-.\   \   \ \ '--.__     '\
+ |  |____.----((Q))\   \__|--\_      \     '
+    ( )        '-'  \_  :  \-' '--.___\
+     Y                \  \  \       \(_)
+     I                 \  \  \         \,
+     I                  \  \  \          \
+     A                   \  \  \          '\
+     |                    \  \__|           '
+                           \_:.  \
+                             \ \  \
+                              \ \  \
+                               \_\_|
+
+"""
+
+    return PlainTextResponse(content=version)
