8875c4b39e9686722a7306a50ffba39785927ba1
Andrey Kononov <zloykaban@gmail.com>
2023-10-13 22:07:57 +0400

    add debug for graceful shutdown

--- start.sh
+++ start.sh
@@ -9,7 +9,6 @@ UVICORN_WORKER_CLASS="uvicorn.workers.UvicornWorker"   # Uvicorn worker class
 APP_MODULE="app.main:app"                              # the application module and app instance
                                                        # AWS profile name
 # Start your FastAPI app with Gunicorn
-#exec gunicorn --workers $NUM_WORKERS --bind 0.0.0.0:8000 -k $UVICORN_WORKER_CLASS $APP_MODULE
 exec uvicorn ${APP_MODULE} --reload --host 0.0.0.0 --port 8000
 term_handler() {
     kill -TERM "$APP_PID" 2>/dev/null
